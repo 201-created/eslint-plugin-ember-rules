41256b66d9985fce1e5cf98ea4041161f7f899a5
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-02-29 16:27:44 -0800

    Add more Ember function prototype extensions
    * observesBefore
    * on
    * Better message for observesBefore

--- tests/lib/rules/no-function-prototype-extension-calls.js
+++ tests/lib/rules/no-function-prototype-extension-calls.js
@@ -15,14 +15,20 @@ ruleTester.run("no-function-prototype-extension-calls", rule, {
   }],
 
   invalid: [{
-    code: "(function() {}.property())",
-    errors: ["Avoid using function prototype extensions like 'property'. Use the computed('dependentKey', function() {}) syntax instead."]
-  }, {
     code: "(function() {}.observes('key'))",
     errors: ["Avoid using function prototype extensions like 'observes'. Use the observer('dependentKey', function() {}) syntax instead."]
   }, {
     code: "(function() {}.observesBefore('key'))",
-    errors: ["Avoid using function prototype extensions like 'observesBefore'. Also, using before observers is deprecated."]
+    errors: ["Avoid using function prototype extensions like 'observesBefore'. Use the beforeObserver('dependentKey', function() {}) syntax instead."]
+  }, {
+    code: "(function() {}.observesImmediately('key'))",
+    errors: ["Avoid using function prototype extensions like 'observesImmediately'. Use the immediateObserver('dependentKey', function() {}) syntax instead."]
+  }, {
+    code: "(function() {}).on('init')",
+    errors: ["Avoid using function prototype extensions like 'on'. Use the on('event', function() {}) syntax instead."]
+  }, {
+    code: "(function() {}.property())",
+    errors: ["Avoid using function prototype extensions like 'property'. Use the computed('dependentKey', function() {}) syntax instead."]
   }, {
     code: "computed(function() {}).property()",
     errors: ["Avoid using function prototype extensions like 'property'. Use the computed('dependentKey', function() {}) syntax instead."]
